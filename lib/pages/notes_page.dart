5aac3de42377c4371d78f19fb32fe491a0757372
JorLojor <nugas123man@gmail.com>
2024-03-21 23:15:17 +0700

    seeep

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todooaap/models/note_database.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -8,16 +10,52 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+
+  //text controller
+  final textController = TextEditingController();
+  //create
+  void createNode(){
+    showDialog(
+        context: context,
+        builder: (context)=>AlertDialog(content: TextField(
+          controller: textController,
+        ),
+          actions: [
+            MaterialButton(onPressed: (){
+              context.read<NoteDatabase>().addNote(textController.text);
+            })
+          ],
+        ),
+    );
+  }
+
+  //read
+
+  //update
+
+  //delete
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFE57373),
-        title: Text("apakek"),
-
+        backgroundColor:const Color(0xFFE57373),
+        title: const Text("apakek"),
       ),
+
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNode,
+
+        child: const Icon(Icons.add),
+      ),
+
+
+
+
 
     );
   }
 }
-
